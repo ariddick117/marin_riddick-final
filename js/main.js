4d67e2d8baa0306a71adf8773345d25e2698d99f
Dana Marin <d_maringarcia@fanshaweonline.ca>
2019-08-04 23:32:24 -0400

    Mobile Index

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 (() => {
-	//this is a self invoking function
-	console.log('My JS issa workin!');
-	
+	console.log('yadayada');
+
+	//variables
+		var button = document.querySelector("#button");
+		var burgerCon = document.querySelector("#burgerContent");
+
+		function hamburgerMenu() {
+			burgerCon.classList.toggle("slideToggle");
+			button.classList.toggle("expanded");
+		}
+
+		button.addEventListener("click", hamburgerMenu, false);
+
 })();

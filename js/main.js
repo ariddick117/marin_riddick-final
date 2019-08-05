f725747648919a6c7c8ce5355b0168e53d23037f
Dana Marin <d_maringarcia@fanshaweonline.ca>
2019-08-05 14:28:18 -0400

    About collapsibles

--- js/main.js
+++ js/main.js
@@ -10,14 +10,37 @@
 		var button = document.querySelector("#button");
 		var burgerCon = document.querySelector("#burgerContent");
 
+	//ABOUT collapsibles variables
+		var colArrowOne = document.querySelector(".arrow_one");
+		var colArrowTwo = document.querySelector(".arrow_two");
+
+		var colContOne = document.querySelector(".collapsible_1_content");
+		var colContTwo = document.querySelector(".collapsible_2_content");
+
+
 	//burger menu function
 		function hamburgerMenu() {
 			burgerCon.classList.toggle("slideToggle");
 			button.classList.toggle("expanded");
 		}
 
+	//ABOUT collapsibles functions
+		function collapsibleOne() {
+			colContOne.classList.toggle("slideToggle");
+			colArrowOne.classList.toggle("expanded");
+		}
+
+		function collapsibleTwo() {
+			colContTwo.classList.toggle("slideToggle");
+			colArrowTwo.classList.toggle("expanded");
+		}
+
 	//burger menu event
 		button.addEventListener("click", hamburgerMenu, false);
+
+	// ABOUT collapsibles functions
+		colArrowOne.addEventListener("click", collapsibleOne, false);
+		colArrowTwo.addEventListener("click", collapsibleTwo, false);
 
 	//carousel function
 		function animateBanners() {
